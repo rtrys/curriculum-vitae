3468558a9a3fb87e5d8a8dd41834e8670b87beb0
Hugo Ortiz <hugo.er.ortiz@gmail.com>
2019-08-18 12:28:33 -0600

    Commit de proyecto base

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>Hola Mundo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto quis, repellendus laboriosam aliquid possimus neque porro cum officia illum fugit adipisci sapiente tenetur, tempora id quos commodi earum corrupti reiciendis.</p>
+  </div>
+);
+
+export default About;
